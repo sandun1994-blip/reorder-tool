c7957f4f456c1e13a7f286b5ba8177e4ac5698c8
Sandun Tharuka <sandun.suntws@gmail.com>
2023-09-16 22:16:56 +0530

    setup auth

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import SignInButton from "./SignInButton"
+
+
+type Props = {}
+
+const AppBar = (props: Props) => {
+  return (
+    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to gray-200 shadow">
+        <Link className="transition-colors hover:text-blue-500" href={'/'}>
+            Home Page
+        </Link>
+
+        <Link className="transition-colors hover:text-blue-500" href={'/dashboard'}>
+           DashBoard
+        </Link>
+        <SignInButton />
+    </header>
+  )
+}
+
+export default AppBar
